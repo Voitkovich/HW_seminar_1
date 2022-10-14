7e4336292195b61a85750f282883800f1c64d8cc
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-14 20:38:14 +0300

    задача_6

---
+++ HW/Задача_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа,которая на вход принимает число и выдает, является ли число четным.
+
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 ==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
