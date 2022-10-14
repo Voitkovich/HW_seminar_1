7a39e5367b8419976a20b177e3a688f2e869ef9d
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-14 19:52:50 +0300

    задача_2

--- HW/Задача_2/Program.cs
+++ HW/Задача_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая пинимает на вход два числа и выдает какое число больше, а какое меньше.
+
+Console.Write("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Число A больше числа B");
+}
+else
+{
+    if (numberA < numberB)
+    Console.WriteLine("Число B больше числа A");
+}
